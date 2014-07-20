548361722baeb175a3c70d8878608e45db7a1cb3
Naohiro Sakuma <n.sakuma@everyleaf.com>
2014-07-20 18:16:19 +0900

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,42 +1,61 @@
+#!/bin/bash
+
 cat <<-EOS
 
   Setting up....
 
 EOS
 
-if [ -f ~/.vimrc ]; then
-  mv ~/.vimrc{,.old}
-fi
+function setup_vim_conf() {
+  if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc{,.old}
+  fi
 
-cd ~/.vim
+  cd ~/.vim
 
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-  git clone git://github.com/Shougo/neobundle.vim.git bundle/neobundle.vim
-fi
+  if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+    git clone https://github.com/Shougo/neobundle.vim.git bundle/neobundle.vim
+  fi
 
-"" Add these colours to ~/.Xresources
-curl https://gist.github.com/w0ng/3278077/raw/4d1d04906349efe4054e314b8a19f027bd537eb1/.Xresources-hybrid > ~/.Xresources
+  # Add these colours to ~/.Xresources
+  curl https://gist.github.com/w0ng/3278077/raw/4d1d04906349efe4054e314b8a19f027bd537eb1/.Xresources-hybrid > ~/.Xresources
 
-if [ ! -x ~/bin ]; then
-  mkdir ~/bin
-fi
-
-if [ ! -x ~/bin/refe ]; then
-  ln -fs ~/.vim/bin/refe ~/bin/refe
-fi
-
-if [ ! -x ~/.vim/docs/rubyrefm ]; then
-  REFERENCE_FILE='ruby-refm-1.9.2-dynamic-20110729'
-  REFERENCE_ARCHIVE_FILE=$REFERENCE_FILE.tar.gz
-  cd /tmp && wget http://doc.okkez.net/archives/201107/$REFERENCE_ARCHIVE_FILE
-  cd /tmp && tar xvzf $REFERENCE_ARCHIVE_FILE
-  mkdir -p ~/.vim/docs/rubyrefm
-  mv /tmp/$REFERENCE_FILE ~/.vim/docs/rubyrefm
-  rm /tmp/$REFERENCE_ARCHIVE_FILE
-fi
-
-cat <<-EOS
+  cat <<-EOS
 
   ... done!
 
 EOS
+}
+
+function update_vim_conf() {
+  local vim_dir=$HOME/.vim
+  # local vim_dir=/tmp/.vim
+  cd $vim_dir
+  result=$(git status)
+  remote_status=$(echo $result | grep "Your branch is up-to-date with 'origin/master'.")
+  commit_status=$(echo $result | grep 'nothing to commit, working directory clean')
+  # if [ ( -n "$remote_status" ) && ( -n "commit_status" ) ]; then
+  if [ -n "$remote_status" ] && [ -n "$commit_status" ]; then
+    git pull origin master
+    if [ -n $? ]; then
+      $vim_dir/bundle/neobundle.vim/bin/neoinstall
+    else
+      echo "!!! Failed repository pull"
+    fi
+  else
+    echo "!!! repository is dirty. check it!"
+    exit 1
+  fi
+}
+
+case "$1" in
+  install)
+    setup_vim_conf
+    ;;
+  update)
+    update_vim_conf
+    ;;
+  *)
+    echo "Unkown command: $1"
+    echo "'install' or 'update'"
+esac
